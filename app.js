b01334fbddc862309fb33e27c1c22cc238a9a1c2
Rizqi Pratama <rev4rmy@gmail.com>
2021-08-10 23:16:47 +0700

    Basic Fucntion Api

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var shortener = require('./routes/v1/shortener');
+var notifRouter = require('./routes/v1/invoice/notification');
 
 var app = express();
 
@@ -19,10 +18,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/v1/shortener', shortener);
+app.use('/v1/invoice/notification', notifRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- config/index.js
+++ config/index.js
@@ -2,9 +2,20 @@ const apiShortener = {
     url: "https://pub-metranet-api.thebigbox.id/URL-Shortener/1.0.0/shortener",
     config :    {
         headers: {
-            "x-api-key": "nfzh3Ww9PezPOmjAZZI0XicuTsq5dg7K",
+            'x-api-key': 'EAT4SwVkXL3ZRlGNUJMiUXzIUGprjvOH', 
+    'Content-Type': 'application/x-www-form-urlencoded'
         }
     }
   } 
 
-module.exports = apiShortener;
+  const apiSMS = {
+    url: "https://api.thebigbox.id/sms-notification/2.0.0/messages",
+    config :    {
+        headers: {
+            'x-api-key': 'EAT4SwVkXL3ZRlGNUJMiUXzIUGprjvOH', 
+    'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    }
+  } 
+
+module.exports ={apiShortener, apiSMS} ;
